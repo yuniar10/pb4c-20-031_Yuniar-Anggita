daeb74251cab7716e42a97c97afed5b2871d61c9
yuniar anggita <anggi4248@gmail.com>
2023-06-01 17:37:09 +0700

    Update Modul 6

---
+++ Modul 6/Tugas Praktikum/BottomNav/app/src/main/java/com/example/bottomnavyt/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.bottomnavyt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.bottomnavyt.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Chat())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.chat -> replaceFragment(Chat())
+                R.id.status -> replaceFragment(Story())
+                R.id.Call -> replaceFragment(Call())
+
+                else ->{
+
+                }
+            }
+
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+    }
+}
